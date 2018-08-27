fc18ced19f9b03739e6c60794a3eeb04e0d604ef
MARIA DE FATIMA ROCHA SILVEIR <081160042@cefsa.corp.local>
2018-08-27 20:08:54 -0300

    initial commit

--- src/FTTFile.java
+++ src/FTTFile.java
@@ -1,14 +1,16 @@
 import java.io.PrintWriter;
+import java.util.Random;
 
 public class FTTFile {
 	public static void main(String[] args) {
 		System.out.println("FTT File...");
 		System.out.println(System.getProperty("user.dir"));
+		Random rn = new Random();
 		try {
-			PrintWriter writer = new PrintWriter("jdados.tsv", "UTF-8");
+			PrintWriter writer = new PrintWriter("jdados.csv", "UTF-8");
 			int contador = 0;
 			do {
-				writer.println(contador);
+				writer.println(contador +";" + rn.nextInt() + ";" + rn.nextFloat());
 				contador++;
 			} while (contador < 100);
 
